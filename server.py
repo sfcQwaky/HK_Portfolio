0f93c8c1b0a74a94d5593dbe7f3f270d7bc80447
qwaky <qwaky87@gmail.com>
2026-08-26 22:18:08 +0200

    phase_1 of project

---
+++ server.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects_overview.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
